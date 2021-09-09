2f8ade843f26f10e1c4c038c87c710e58cd0c626
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-09-09 13:12:11 +0900

    # refactor : 중복 코드 제거 및 CSS 정정

--- 1-Colors/src/js/App.js
+++ 1-Colors/src/js/App.js
@@ -1,19 +1,17 @@
 export default function App({ $target }) {
+  const $background = document.getElementById('app')
   const $button = document.createElement('button')
   $button.className = 'color-btn'
   $button.innerText = 'Click me'
   $target.appendChild($button)
 
-  $button.addEventListener('click', (e) => {
-    colorChange()
+  $button.addEventListener('click', () => {
+    onColorChange()
   })
-}
 
-function colorChange() {
-  const color = ['#FC5C7D', '#6A82FB', '#38ef7d', '#fffbd5']
-
-  let num = Math.floor(Math.random() * color.length)
-
-  const $background = document.getElementById('app')
-  $background.style.backgroundColor = color[num]
+  function onColorChange() {
+    const colors = ['#FC5C7D', '#6A82FB', '#38ef7d', '#fffbd5', 'eee8aa', '#ffdab9', '#fffaf0']
+    let num = Math.floor(Math.random() * colors.length)
+    $background.style.backgroundColor = colors[num]
+  }
 }
